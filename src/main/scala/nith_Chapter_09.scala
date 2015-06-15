cca4247cdfc9deae59a537a9281b691930b4d952
Nikolaus Thiel <nikolaus.thiel@leonteq.com>
2015-06-15 17:52:12 +0200

    Exercise 9.2 done

--- src/main/scala/nith_Chapter_09.scala
+++ src/main/scala/nith_Chapter_09.scala
@@ -9,13 +9,11 @@ object Ch9 {
 
     def string(s: String): Parser[String]
 
-    def char(c: Char): Parser[Char] =
-      string(c.toString) map (_.charAt(0))
+    def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
-    def succeed[A](a: A): Parser[A] =
-      string("") map (_ => a)
+    def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
-    def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+    def or[A,B >: A](p1: Parser[A])(p2: Parser[B]): Parser[A]
 
     def many[A](p: Parser[A]): Parser[List[A]]
 
@@ -23,31 +21,59 @@ object Ch9 {
 
     def slice[A](p: Parser[A]): Parser[String]
 
+    def product[A,B](p1: Parser[A])(p2: Parser[B]): Parser[(A,B)]
 
-    implicit def operators[A](p: Parser[A]): ParserOps[A] =
-      ParserOps[A](p)
+    // 9.1 Using product, implement the now-familiar combinator map2 and then use this to implement many1 in terms of
+    // many. Note that we could have chosen to make map2 primitive and defined product in terms of map2 as we’ve done
+    // in previous chapters. The choice is up to you.
+    def map2[A,B,C](p1: Parser[A])(p2: Parser[B])(f: (A, B) => C): Parser[C] =
+      product[A,B](p1)(p2).map(x => f(x._1,x._2))
 
-    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
-      ParserOps(f(a))
+    def many1[A](p: Parser[A]): Parser[List[A]] = p.product[List[A]](many[A](p)).map[List[A]](_._2)
+
+
+    implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+
+    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
     case class ParserOps[A](p: Parser[A]) {
-      def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p,p2)
-      def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
+      def or[B >: A](p2: => Parser[B]): Parser[B] = self.or[A,B](p)(p2)
+      def |[B >: A](p2: Parser[B]): Parser[B] = or(p2)
+
       def many[B >: A]: Parser[List[B]] = self.many(p)
+      def many1[B >: A]: Parser[List[B]] = self.many1(p)
+
       def map[B](f: A => B): Parser[B] = self.map(p)(f)
+      def map2[B,C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2[A,B,C](p)(p2)(f)
+
+      def product[B](p2: Parser[B]): Parser[(A,B)] = self.product[A,B](p)(p2)
+      def **[B](p2: Parser[B]): Parser[(A,B)] = product[B](p2)
     }
 
     val numA: Parser[Int] = char('a').many.map(l => List.length(l))
+    val numA1: Parser[Int] = char('a').many1.map(l => List.length(l))
 
     object Laws {
-      import Ch08.Phase2._
+      import Ch08.Phase2.Gen
+      import Ch08.Phase3.{forAll,forAllAll,Prop}
 
-      def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      final def equal[A](p1: Parser[A])(p2: Parser[A])(in: Int => Gen[String]): Prop =
         forAll(in)(s => run(p1)(s) == run(p2)(s))
 
-      def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
-        equal(p, p.map(a => a))(in)
+      final def mapLaw[A](p: Parser[A])(in: Int => Gen[String]): Prop = equal(p)(p.map(a => a))(in)
+
+      //9.2 Hard: Try coming up with laws to specify the behavior of product.
+      final def prodAssociative[A](p1: Parser[A])(p2: Parser[A])(p3: Parser[A])(in: Int => Gen[String]): Prop =
+        equal[(A,(A,A))](p1.**(p2.**(p3)))(p1.**(p2).**(p3).map[(A,(A,A))]( x => (x._1._1,(x._1._2,x._2))))(in)
+
+      final def prodNeutralElement[A](p: Parser[A])(aGen: Int => Gen[A])(in: Int => Gen[String]): Prop =
+        forAllAll[A](aGen)(a => equal[A](p)(p.**(succeed[A](a)).map[A](x => x._1))(in))
+
+      final def prodCommutative[A](p1: Parser[A])(p2: Parser[A])(in: Int => Gen[String]): Prop =
+        equal[(A,A)](p1.**(p2))(p1.**(p2))(in)
+
     }
+
   }
 
 
